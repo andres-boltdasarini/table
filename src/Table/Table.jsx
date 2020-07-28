3b98917075f29f7e153e4791fecc05e14d311825
andres-boltdasarini <lau47@mail.ru>
2020-07-28 14:52:09 +0300

    4but

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -7,22 +7,43 @@ import {requestUsers} from "../redux/tableReducer";
 
 
 class Table extends React.Component {
+    state = {
+        buttext:'down',
+        sorti:false,
+        status:this.props.status
+    }
+    buttontextupdate = () => {
+        this.setState({
+            buttext:'up'
+        })
+    }
+    activateSorti = () =>{
+        this.setState({
+            sorti:true
+        })}
+    deactivateSorti(){
+        this.setState({
+            sorti:false
+        })}
     componentDidMount() {
         this.props.requestUsers()
     }
     render() {
         let postsElementsId = this.props.items.map( p => p.id).sort(function (a, b) {return a - b}).reverse().map( p => <div>{p}</div>)
         let postsElementsFirstName = this.props.items.map( p => p.firstName).sort().reverse().map( p => <div>{p}</div>)
+        let postsElementsFirstNamerev = this.props.items.map( p => p.firstName).sort().map( p => <div>{p}</div>)
         return  <>
-           { this.props.isFetching ? <Preloader /> : null }
+           {this.props.isFetching ? <Preloader /> : null}
            <table border="1">
                 <tr>
                     <th>Id</th>
-                    <th>FirstName</th>
+                    <th>FirstName <button onClick={this.activateSorti} >{this.state.buttext}</button></th>
                 </tr>
                 <tr>
                     <td>{postsElementsId}</td>
-                    <td> {postsElementsFirstName}</td>
+                    <td>{!this.state.sorti && postsElementsFirstName}
+                        {this.state.sorti && postsElementsFirstNamerev}
+                    </td>
                 </tr>
             </table>
         </>
