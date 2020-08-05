5cce8d49a5d7194db4c782093797913d0c96bc53
andres-boltdasarini <lau47@mail.ru>
2020-08-05 16:12:30 +0300

    6

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
  class Table extends React.Component {
-        state = {
-            buttext:'down',
-            sorti:false,
-            status:this.props.status
-        }
-
+     constructor() {
+         super();
+         this.state = {
+             buttext: 'down',
+             sorti: false,
+             currentPage: 3,
+             todosPerPage: 3
+         }
+         this.handlClick = this.handlClick.bind(this);
+     }
+     handlClick(event) {
+         this.setState({
+             currentPage: Number(event.target.id)
+         });
+     }
         activateSorti = () => {
             this.setState({
                 sorti:true
@@ -14,13 +23,29 @@ import React from 'react';
 
      render() {
 
-         let postsElementsId = this.props.items.map(p => p.id).sort(function (a, b) {
-             return a - b
-         }).reverse().map(p => <div>{p}</div>)
-         let postsElementsFirstName = this.props.items.map(p => p.firstName).sort().reverse().map(p => <div>{p}</div>)
-         let postsElementsFirstNamerev = this.props.items.map(p => p.firstName).sort().map(p => <div>{p}</div>)
+         let postsElementsId = this.props.items.map(p => p.id).sort(function (a, b) {return a - b}).reverse().map(p => <div>{p}</div>)
+         let postsElementsFirstName = this.props.items.map(p => p.firstName).sort().reverse()
+         const indexOfLastTodo = this.state.currentPage * this.state.todosPerPage
+         const indexOfFirstTodo = indexOfLastTodo - this.state.todosPerPage
+         const currentTodos = postsElementsFirstName.slice(indexOfFirstTodo, indexOfLastTodo).map(p => <div>{p}</div>)
 
+         const pageNumbers = [];
+         for (let i = 1; i <= Math.ceil(postsElementsFirstName.length / this.state.todosPerPage); i++) {
+             pageNumbers.push(i);
+         }
+         const renderPageNumbers = pageNumbers.map(number => {
+             return (
+                 <li
+                     key={number}
+                     id={number}
+                     onClick={this.handlClick}
+                 >
+                     {number}
+                 </li>
+             );
+         })
          return <>
+             {renderPageNumbers}
              <table border="1">
                  <tr>
                      <th>Id</th>
@@ -29,7 +54,7 @@ import React from 'react';
                  <tr>
                      <td>{postsElementsId}</td>
                      <td>{!this.state.sorti && postsElementsFirstName}
-                         {this.state.sorti && postsElementsFirstNamerev}
+                         {this.state.sorti && currentTodos}
                      </td>
                  </tr>
              </table>
