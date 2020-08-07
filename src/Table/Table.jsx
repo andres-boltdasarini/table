ebdebdafe409d6a847b1ec09c3e6f8fd3f07e2db
andres-boltdasarini <lau47@mail.ru>
2020-08-07 10:45:05 +0300

    7autoren

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -17,22 +17,23 @@ import React from 'react';
          });
      }
         activateSorti = () => {
+
             this.setState({
                 sorti:true
             })}
 
      render() {
-         const atemA = this.props.items.sort((a, b) => {if(a.firstName < b.firstName ) return -1
-             if(a.firstName  > b.firstName ) return 1}).map(p =><div>{p.id}</div> )
-
-         const atem1 = this.props.items.sort((a, b) => (a.id - b.id)).map(p =><div>{p.id}</div> )
 
          const result = Object.values(this.props.items).map(v => Object.values(v)).map(p =><div>{p[0]}</div>)
-         let postsElementsId = this.props.items.map(p => p.id).sort(function (a, b) {return a - b}).reverse().map(p => <div>{p}</div>)
-         let postsElementsFirstName = this.props.items.map(p => p.firstName).sort().reverse()
+         let postsElementsId = this.props.items.map(p => <div>{p.id}</div>)
+         let postsElementsFirstNamerev = this.props.items.map(p => p.firstName).reverse().map(p =><div>{p}</div>)
+         let postsElementsFirstName = this.props.items.map(p => p.firstName).map(p =><div>{p}</div>)
          const indexOfLastTodo = this.state.currentPage * this.state.todosPerPage
          const indexOfFirstTodo = indexOfLastTodo - this.state.todosPerPage
          const currentTodos = postsElementsFirstName.slice(indexOfFirstTodo, indexOfLastTodo).map(p => <div>{p}</div>)
+         const atem1 = this.props.items.sort((a, b) => (a.id - b.id)).map(p =><div>{p.id}</div> )
+         const atemA = this.props.items.sort((a, b) => {if(a.firstName < b.firstName ) return -1
+             if(a.firstName  > b.firstName ) return 1}).map(p =><div>{p.id}</div> )
 
          const pageNumbers = [];
          for (let i = 1; i <= Math.ceil(postsElementsFirstName.length / this.state.todosPerPage); i++) {
@@ -50,11 +51,19 @@ import React from 'react';
              );
          })
 
-
-
          return <>
-
-         <div>{atem1}</div>
+             <table border="1">
+                 <tr>
+                     <th>Id</th>
+                     <th>FirstName <button onClick={this.activateSorti} >{this.state.buttext}</button></th>
+                 </tr>
+                 <tr>
+                     <td>{postsElementsId}</td>
+                     <td>{!this.state.sorti && postsElementsFirstName}
+                         {this.state.sorti && postsElementsFirstNamerev}
+                     </td>
+                 </tr>
+             </table>
          </>
      }
 }
