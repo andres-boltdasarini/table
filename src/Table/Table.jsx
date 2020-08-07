1e73aff47d65d22347a54433cb260bc242dda044
andres-boltdasarini <lau47@mail.ru>
2020-08-07 17:44:48 +0300

    7pag

--- src/Table/Table.jsx
+++ src/Table/Table.jsx
@@ -25,33 +25,42 @@ import React from 'react';
      render() {
 
          const result = Object.values(this.props.items).map(v => Object.values(v)).map(p =><div>{p[0]}</div>)
-         let postsElementsId = this.props.items.map(p => <div>{p.id}</div>)
+
+
          let postsElementsFirstNamerev = this.props.items.map(p => p.firstName).reverse().map(p =><div>{p}</div>)
-         let postsElementsFirstName = this.props.items.map(p => p.firstName).map(p =><div>{p}</div>)
+
+
+
          const indexOfLastTodo = this.state.currentPage * this.state.todosPerPage
          const indexOfFirstTodo = indexOfLastTodo - this.state.todosPerPage
-         const currentTodos = postsElementsFirstName.slice(indexOfFirstTodo, indexOfLastTodo).map(p => <div>{p}</div>)
+
+         let postsElementsFirstName = this.props.items.map(p => <div>{p.firstName}</div>).slice(indexOfFirstTodo, indexOfLastTodo)
+         let postsElementsId = this.props.items.map(p => <div>{p.id}</div>).slice(indexOfFirstTodo, indexOfLastTodo)
+
          const atem1 = this.props.items.sort((a, b) => (a.id - b.id)).map(p =><div>{p.id}</div> )
          const atemA = this.props.items.sort((a, b) => {if(a.firstName < b.firstName ) return -1
              if(a.firstName  > b.firstName ) return 1}).map(p =><div>{p.id}</div> )
 
          const pageNumbers = [];
-         for (let i = 1; i <= Math.ceil(postsElementsFirstName.length / this.state.todosPerPage); i++) {
+         for (let i = 1; i <= Math.ceil(this.props.items.length / this.state.todosPerPage); i++) {
              pageNumbers.push(i);
          }
          const renderPageNumbers = pageNumbers.map(number => {
              return (
-                 <li
+                 <span
                      key={number}
                      id={number}
                      onClick={this.handlClick}
                  >
                      {number}
-                 </li>
+                 </span>
              );
          })
 
          return <>
+
+                     {renderPageNumbers}
+
              <table border="1">
                  <tr>
                      <th>Id</th>
@@ -59,9 +68,7 @@ import React from 'react';
                  </tr>
                  <tr>
                      <td>{postsElementsId}</td>
-                     <td>{!this.state.sorti && postsElementsFirstName}
-                         {this.state.sorti && postsElementsFirstNamerev}
-                     </td>
+                     <td>{postsElementsFirstName}</td>
                  </tr>
              </table>
          </>
