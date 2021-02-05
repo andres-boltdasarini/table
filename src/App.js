562bf2d2036c60ea4d6f584c71bcd8911fdd632f
andres-boltdasarini <lau47@mail.ru>
2021-02-05 11:50:21 +0300

    table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import  axios from "axios";
 import Loader from "./Loader/Loader";
+import Table from "./Table/Table";
 
 
 class App extends Component {
-
+    state ={
+        isLoading: true,
+        data: [],
+    }
     async componentDidMount() {
+
         const response = await axios.get(` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
         const data = await response.data
-        console.log(data)
-
+        this.setState({
+            isLoading: false,
+            data
+        })
     }
-
     render() {
         return (
             <div className="container">
-                MyApp
-                <Loader/>
+                {
+                    this.state.isLoading
+                        ? <Loader />
+                        : <Table
+                            data={this.state.data}
+                        />
+                }
             </div>
         );
     }
