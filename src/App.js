fe391367b348605183b8ed0d459bf26096f18a4c
andres-boltdasarini <lau47@mail.ru>
2021-02-11 13:44:28 +0300

    paginator

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import _ from 'lodash';
 import {DetailRowView} from "./DetailRowView/DetailRowView";
 import {ModeSelector} from "./ModeSelector/ModeSelector";
 import axios from "axios";
+import ReactPaginate from 'react-paginate';
 
 
 class App extends Component {
@@ -15,6 +16,8 @@ class App extends Component {
         sort: 'asc',  // 'desc'
         sortField: 'id',
         row: null,
+        pageSize: 50,
+        currentPage: 0,
     }
 
     async fetchData(url) {
@@ -28,17 +31,12 @@ class App extends Component {
 
     }
     onSort = sortField => {
-
         const cloneData = this.state.data.concat();
         const sort = this.state.sort === 'asc' ? 'desc' : 'asc';
         const data = _.orderBy(cloneData, sortField, sort);
-
-        this.setState({
-            data,
-            sort,
-            sortField
-        })
+        this.setState({data, sort, sortField})
     }
+
     modeSelectHandler = url => {
         // console.log(url)
         this.setState({
@@ -53,6 +51,10 @@ class App extends Component {
     onRowSelect = row => (
         this.setState({row})
     )
+    pageChangeHandler = ({selected}) => (
+        this.setState({currentPage: selected})
+    )
+
     render() {
         if(!this.state.isModeSelected){
             return (
@@ -61,18 +63,42 @@ class App extends Component {
                 </div>
             )
         }
+        const displayData = _.chunk(this.state.data, this.state.pageSize)[this.state.currentPage]
         return (
             <div className="container">
                 {
                     this.state.isLoading
                         ? <Loader />
                         : <Table
-                            data={this.state.data}
+                            data={displayData}
                             onSort={this.onSort}
                             sort={this.state.sort}
                             sortField={this.state.sortField}
                             onRowSelect={this.onRowSelect}
                         />
+                }
+                {
+
+                    this.state.data.length > this.state.pageSize
+                        ? <ReactPaginate
+                            previousLabel={'<'}
+                            nextLabel={'>'}
+                            breakLabel={'...'}
+                            breakClassName={'break-me'}
+                            pageCount={20}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={5}
+                            onPageChange={this.pageChangeHandler}
+                            containerClassName={'pagination'}
+                            activeClassName={'active'}
+                            pageClassName="page-item"
+                            pageLinkClassName="page-link"
+                            previousClassName="page-item"
+                            nextClassName="page-item"
+                            previousLinkClassName="page-link"
+                            nextLinkClassName="page-link"
+                            forcePage={this.state.currentPage}
+                        /> : null
                 }
                 {
                     this.state.row ? <DetailRowView person={this.state.row} /> : null
